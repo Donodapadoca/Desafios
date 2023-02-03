b7b08c5666f492d1ff3b810c82cf1a02ca7086c2
Donodapadoca <joao.timao@hotmail.com>
2023-02-03 15:11:36 -0300

    def novo

--- src/Def1.java
+++ src/Def1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Def1 {
 
@@ -12,6 +13,53 @@ public class Def1 {
 
         System.out.printf("o valor do 1º desafio é: " + soma);
         System.out.println("\n");
+        // segundo desafio
+        int a = 1, b = 0, c = 0, v = 0, i2;
+
+        Scanner d2 = new Scanner(System.in);
+
+        System.out.println("Digite um valor de 1 a 100 para o 2º desafio:");
+        i2 = d2.nextInt();
+
+        for (int i = 0; i < 100; i++) {
+
+            c = a;
+            a = a + b;
+            b = c;
+
+            if (i2 == c) {
+                v++;
+            }
+
+        }
+        if (v == 1) {
+            System.out.println("Ele faz parte da sequencia e aparece: " + v + " vez");
+        } else if (v >= 2) {
+            System.out.println("Ele faz parte da sequencia e aparece: " + v + " vezes");
+        } else {
+            System.out.println("Ele não faz parte da sequencia");
+        }
+
+
+        //terceiro desafio
+
+
+
+        //quarto desafio
+
+        
+
+        // quinto desafio
+        Scanner n = new Scanner(System.in);
+        System.out.println("\ndigite seu nome: ");
+        String nome = n.next();
+        String emon = "";
+
+        for (int i = nome.length() - 1; i >= 0; i--) {
+            emon = emon + nome.charAt(i);
+
+        }
+        System.out.println(emon);
 
     }
 
--- src/Def3.java
+++
@@ -1,6 +0,0 @@
-public class Def3 {
-
-    public void main(String[] args) {
-
-    }
-}
